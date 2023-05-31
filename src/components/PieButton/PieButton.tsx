99910a9c3fff66e91f18167aa5d73a5172bca68f
davidbolaji <odavidbolaji14@gmail.com>
2023-05-31 23:16:00 +0100

    Add what we do section 3

--- src/components/PieButton/PieButton.tsx
+++ src/components/PieButton/PieButton.tsx
@@ -1,42 +1,46 @@
-import React, { useRef, useState } from "react";
+import React from "react";
 import { GiTeacher } from "react-icons/gi";
 import { RiParentFill } from "react-icons/ri";
 import { FaSchool } from "react-icons/fa";
 import "./PieButton.css";
-import { Button, Tour, TourProps } from "antd";
+import { Button, notification } from "antd";
 
 const PieButton: React.FC = () => {
-  const ref1 = useRef(null);
-  const ref2 = useRef(null);
-  const ref3 = useRef(null);
-  const [open, setOpen] = useState<boolean>(false);
+  const [api, contextHolder] = notification.useNotification();
 
-  const steps: TourProps["steps"] = [
-    {
-      title: "Teachers",
+  const d = () => {
+    api.open({
+      key: "d1",
+      message: "Teachers",
+      placement: "bottom",
       description:
-        "Turn 300k+ teachers to skilled and progressive models for learners, thereby achieving broadened career opportunities, professional fulfillment in the short and long term and outstanding learners performance.",
-      // placement: "right",
-      cover: <GiTeacher size={30} color={"white"} />,
-      target: () => ref1.current,
-    },
-    {
-      title: "Parents",
+        " –Turn 300k+ teachers to skilled and progressive models for learners, thereby achieving broadened career opportunities, professional fulfillment in the short and long term and outstanding learners performance.",
+      icon: <GiTeacher size={30} color={"white"} />,
+    });
+  };
+
+  const d2 = () => {
+    api.open({
+      key: "d2",
+      message: "Parents",
+      placement: "topLeft",
       description:
-        "Equip 500+ parents to effectively support the academic and general development of learners.",
-      // placement: "bottom",
-      cover: <RiParentFill color="white" size={30} />,
-      target: () => ref2.current,
-    },
-    {
-      title: "Schools",
-      // placement: "left",
-      cover: <FaSchool ref={ref3} color="white" size={30} />,
+        " –Equip 500+ parents to effectively support the academic and general development of learners.",
+      icon: <GiTeacher size={30} color={"white"} />,
+    });
+  };
+
+  const d3 = () => {
+    api.open({
+      key: "d1",
+      message: "Schools",
+      placement: "topRight",
       description:
-        "Empower 10k+ school administrators to create an enabling environment for developing academic excellence and high life performance in learners",
-      target: () => ref3.current,
-    },
-  ];
+        " –Empower 10k+ school administrators to create an enabling environment for developing academic excellence and high life performance in learners",
+      icon: <FaSchool color="white" size={30} />,
+    });
+  };
+
   return (
     <div className="flex items-center justify-center bg-[#fafafa] md:mx-20 mx-5 py-40 ">
       <div className="relative rounded-full overflow-clip shadow-2xl">
@@ -45,31 +49,20 @@ const PieButton: React.FC = () => {
           className="w-72 h-72 rounded-full circle bg-[#fafafa] shadow"
         ></div>
         <div className="absolute bottom-9 right-[110px] cursor-pointer animate__animated animate__flash animate__slow animate__infinite 	infinite">
-          <Button
-            onClick={() => setOpen(true)}
-            className="border-none"
-            ref={ref1}
-          >
+          <Button onClick={() => d()} className="border-none">
             <GiTeacher size={30} color={"blue"} />
           </Button>
         </div>
         <div className="cursor-pointer trans2 absolute w-3 rotate-[65deg] h-[168px] translate-x-16 -translate-y-[190px] shadow-2xl bg-[#fff]"></div>
-        <div className="absolute top-[80px] left-[45px] cursor-pointer">
-          <Button
-            onClick={() => setOpen(true)}
-            className="border-none"
-            ref={ref2}
-          >
+        <div className="absolute top-[80px] left-[45px] cursor-pointer  animate__animated animate__flash animate__slow animate__infinite 	infinite">
+          <Button onClick={() => d2()} className="border-none">
             <RiParentFill color="blue" size={30} />
           </Button>
         </div>
-        <div className="cursor-pointer trans2 bg-[#fff] absolute w-3 h-32 right-0 z-30 rotate-[120deg] -translate-x-16 -translate-y-[165px] shadow-2xl"></div>
-        <div className="absolute right-[40px] top-[80px] cursor-pointer">
-          <Button
-            onClick={() => setOpen(true)}
-            className="border-none"
-            ref={ref3}
-          >
+        <div className="cursor-pointer trans2 bg-[#fff] absolute w-3 h-32 right-0 z-20 rotate-[120deg] -translate-x-16 -translate-y-[165px] shadow-2xl"></div>
+        <div className="absolute right-[40px] top-[80px] cursor-pointer  animate__animated animate__flash animate__slow animate__infinite 	infinite">
+          <Button onClick={() => d3()} className="border-none">
+            {/* // @ts-ignore */}
             <FaSchool color="blue" size={30} />
           </Button>
         </div>
@@ -78,14 +71,7 @@ const PieButton: React.FC = () => {
       </div>
       {/* <div id="my-pie-chart two"></div>
       <div id="my-pie-chart three"></div> */}
-
-      <Tour
-        type="primary"
-        open={open}
-        onClose={() => setOpen(false)}
-        steps={steps}
-        // mask={false}
-      />
+      {contextHolder}
     </div>
   );
 };
