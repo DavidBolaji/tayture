af52c348c4cfbeac9a96614e4f96b496ba360781
davidbolaji <odavidbolaji14@gmail.com>
2023-06-01 08:35:57 +0100

    adjusting overflow

--- src/components/PieButton/PieButton.tsx
+++ src/components/PieButton/PieButton.tsx
@@ -43,30 +43,27 @@ const PieButton: React.FC = () => {
 
   return (
     <div className="flex items-center justify-center md:mx-20 mx-5 py-40 ">
-      <div className="relative rounded-full overflow-hidden">
-        <div
-          id=""
-          className="w-72 h-72 rounded-full circle bg-[#fafafa] shadow overflow-hidden"
-        ></div>
+      <div className="relative rounded-full  shadow-2xl ">
+        <div id="" className="w-72 h-72 rounded-full circle bg-[#fafafa]"></div>
         <div className="absolute bottom-9 right-[110px] cursor-pointer animate__animated animate__flash animate__slow animate__infinite 	infinite">
           <Button onClick={() => d()} className="border-none">
             <GiTeacher size={30} color={"blue"} />
           </Button>
         </div>
-        <div className="cursor-pointer trans2 absolute w-3 rotate-[65deg] h-[168px] translate-x-16 -translate-y-[190px] shadow-2xl bg-[#fff]"></div>
+        <div className="cursor-pointer trans2 absolute w-3 rotate-[65deg] h-[124px] translate-x-16 -translate-y-[170px] shadow-2xl bg-[#fff]"></div>
         <div className="absolute top-[80px] left-[45px] cursor-pointer  animate__animated animate__flash animate__slow animate__infinite 	infinite">
           <Button onClick={() => d2()} className="border-none">
             <RiParentFill color="blue" size={30} />
           </Button>
         </div>
-        <div className="cursor-pointer trans2 bg-[#fff] absolute w-3 h-32 right-0 z-20 rotate-[120deg] -translate-x-16 -translate-y-[165px] shadow-2xl"></div>
+        <div className="cursor-pointer trans2 bg-[#fff] absolute w-3 h-[122px] right-0 z-20 rotate-[120deg] -translate-x-16 -translate-y-[165px] shadow-2xl"></div>
         <div className="absolute right-[40px] top-[80px] cursor-pointer  animate__animated animate__flash animate__slow animate__infinite 	infinite">
           <Button onClick={() => d3()} className="border-none">
             {/* // @ts-ignore */}
             <FaSchool color="blue" size={30} />
           </Button>
         </div>
-        <div className="cursor-pointer trans2 absolute w-3 h-40 shadow-2xl bg-[#fff] -top-5 left-[48%]"></div>
+        <div className="cursor-pointer trans2 absolute w-3 h-[125px] shadow-2xl bg-[#fff] -top-0 left-[48%]"></div>
         <div className="trans w-12 h-12 rounded-full bg-[#fff] absolute top-[120px] left-[120px]  border-none"></div>
       </div>
       {/* <div id="my-pie-chart two"></div>
