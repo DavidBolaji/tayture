485bc2b03a8e666c6365636b5acbbe983858589a
davidbolaji <odavidbolaji14@gmail.com>
2023-06-04 17:46:01 +0100

    add editor

--- src/AdminService/AuthLayer.tsx
+++ src/AdminService/AuthLayer.tsx
@@ -1,14 +1,14 @@
-import React, { useEffect } from "react";
-import { useSelector } from "react-redux";
-import { useNavigate } from "react-router-dom";
+import React from "react";
+// import { useSelector } from "react-redux";
+// import { useNavigate } from "react-router-dom";
 
 interface Props {
   children: React.ReactNode;
 }
 
 const AuthLayer: React.FC<Props> = ({ children }) => {
-  const user: any = useSelector((state: any) => state.user.user);
-  const navigate = useNavigate();
+  // const user: any = useSelector((state: any) => state.user.user);
+  // const navigate = useNavigate();
 
   // useEffect(() => {
   //   if (typeof user?.name === "undefined") {
